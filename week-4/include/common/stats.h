e094483ca38db05359554e79784b7e821c7bfdd2
Matt Cooper <m.cooper44@gmail.com>
2022-01-08 16:00:10 -0500

    base commit for week 4 with basic file tree and files

---
+++ week-4/include/common/stats.h
@@ -0,0 +1,134 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file  stats.h
+ * @brief  function prototypes for basic stats assignment
+ *
+ * These are prototypes for the week 1 warm up assignment for
+ * Introduction to Embedded Systems Software and Development Environments
+ * 
+ *
+ * @author mcooper
+ * @date December 2021 
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief  prints summary statistics of the provided array
+ * 	   to std out
+ *
+ * This function prints the various stats determined 
+ * by the other functions 
+ * min, max, mean and median
+ *
+ * @param minimum: the min of the array
+ * @param max: the max of the array
+ * @param mean: the mean of the array
+ * @param median: the median of the array
+ *
+ * @return void
+ */
+
+void print_statistics(unsigned char minimum, unsigned char maximum, unsigned char mean, unsigned char median);
+
+/**
+ * @brief prints an arbitrary array of numbers
+ *
+ * this function loops through an array of numbers and prints them
+ * to the terminal
+ *
+ * @param array: the starting point of the array to be printed
+ * @param counter: the length of the array 
+ * @return  void
+ */
+
+void print_array(unsigned char *array, unsigned int counter);
+
+/**
+ * @brief find the median of an arbitrary array of numbers
+ *
+ * Finds the median of a sorted array of integers.  The median
+ * is the number in a sorted array that is the middle value.
+ * In an even array you can find the middle value by dividing
+ * length of the array by two.  Otherwise, you will need to
+ * average the middle two values
+ *
+ * @param array: an array of integers
+ * @param counter: the number of integers in the array
+ *
+ * @return float
+ */
+
+unsigned char find_median(unsigned char *array, unsigned int counter);
+
+/**
+ * @brief find the mean of an arbitrary array of numbers
+ *
+ * The mean is calculated by adding the values in the array
+ * and dividing that sum by the number of elements in the array
+ *
+ * @param array: an array of integers
+ * @param counter: the number of elements in the array
+ *
+ * @return unsigned char
+ */
+
+float find_mean (unsigned char *array, unsigned int counter);
+
+/**
+ * @brief  finds the max of an array of numbers
+ *
+ * Iterates through the array and finds the minimum 
+ * value through an element wise comparison of each
+ * element 
+ *
+ * @param array: an array of integers
+ * @param counter: the number of elements in the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_maximum (unsigned char *array, unsigned int counter);
+
+/**
+ * @brief  finds the min of an array of numbers
+ *
+ * Iterates through the array and finds the maximum 
+ * value through an element wise comparison of each
+ * element 
+ *
+ * @param array: an array of integers
+ * @param counter: the number of elements in the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_minimum (unsigned char *array, unsigned int counter);
+
+
+/**
+ * @brief sorts an arbitrary array of numbers from max to min 
+ *
+ * this implementation uses bubble sort to sort the array
+ * in descending order 
+ *
+ * @param array:  pointer to an array
+ * @param counter: the number of elements in the array
+ *
+ * @return void
+ */
+
+void sort_array (unsigned char *array, unsigned int counter);
+
+#endif /* __STATS_H__ */
---
+++ week-4/src/stats.c
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file Stats.c 
+ * @brief a bare bones command line stats program
+ *
+ * This is a basic C lang exercise to test and re-enforce basic skills
+ * for completion of Introduction to Embedded Systems Course on Coursera
+ * 
+ * It takes an array, sorts it and prints basic stats such as mean, median, minimum
+ * and maximum. 
+ *
+ * @author mcooper
+ * @date December 11 2021
+ *
+ */
+
+#include <stdio.h>
+#include "stats.h" // prototypes of stats functions to sort, determine min, max
+		   // median of an array of numbers
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main(){
+  // array of numbers to operate on
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* These variables will hold the values returned by the functions  */
+  unsigned char mean = 0;
+  unsigned char median = 0;
+  unsigned char min = 0;
+  unsigned char max = 0;
+
+  /* calling functions defined in stats.h */
+  printf("Initial array state: \n");
+  print_array(test, SIZE);
+
+  sort_array(test, SIZE);
+  median = find_median(test, SIZE);
+  mean = find_mean(test, SIZE);
+  min = find_minimum(test, SIZE);
+  max = find_maximum(test, SIZE);
+  print_statistics(min, max, mean, median);
+  printf("Final state of array after sorting: \n");
+  print_array(test, SIZE);
+}
+
+/* implementation of the protyped functions in stats.h */
+
+void print_statistics(unsigned char min, unsigned char max, unsigned char mean, unsigned char median){
+  printf("The minimum of the array is %d \n", min);
+  printf("The maximum of the array is %d \n", max);
+  printf("The median of the array is %d\n", median);
+  printf("The mean of the array is %d\n\n", mean);
+}
+
+void print_array(unsigned char *array, unsigned int counter){
+  for (int i=0;i<counter/8; i++){
+    for(int j=0;j<counter/5; j++){
+      printf("%3d   ",array[8*i+j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  return;
+}
+
+void sort_array (unsigned char *array, unsigned int counter){
+  int i=0, j=0, temp = 0;
+  for(i=0; i<counter; i++)
+
+  {
+    for(j=0; j<counter-i; j++)
+    {
+      if(array[j]<array[j+1])
+      {
+        temp=array[j];
+        array[j]=array[j+1];
+        array[j+1]=temp;
+      }
+    }
+  }
+}
+
+unsigned char find_median(unsigned char *array, unsigned int counter){
+  float median=0;
+    if(counter%2==0)
+      median=(array[(counter-1)/2] + array[counter/2])/2.0;
+    else
+      median = array[counter/2];
+    return (unsigned char)(median);
+}
+
+float find_mean(unsigned char *array, unsigned int counter){
+  unsigned int adder = 0;
+  for(int i = 0; i < counter; i++){
+    adder += array[i];
+  }
+  return (unsigned char)(adder / counter); 
+}
+
+unsigned char find_minimum(unsigned char *array, unsigned int counter){
+  unsigned char min = *array;
+  for (int i=1; i<counter;i++){
+  if (*(array +i) < min){
+    min = *(array +i);
+    }
+  }
+  return min;
+}
+
+unsigned char find_maximum(unsigned char *array, unsigned int counter){
+  unsigned char max = *array;
+  for (int i=1; i<counter;i++){
+  if (*(array +i) > max){
+    max = *(array +i);
+    }
+  }
+  return max;
+}
