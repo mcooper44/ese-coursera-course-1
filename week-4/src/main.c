92821f7af4d604c73df2dcbfc52a8996e0131ca9
Matt Cooper <m.cooper44@gmail.com>
2022-01-11 22:55:11 -0500

    adding main function with compile time COURSE1 macro

---
+++ week-4/src/main.c
@@ -0,0 +1,30 @@
+
+/******************************************************************************
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file  main.c
+ * @brief 
+ *
+ * 
+ * 
+ * 
+ * 
+ *
+ * @author mcooper
+ * @date January 2022
+ *
+ */
+
+int main{
+  
+  #ifdef COURSE1
+    course1();
+  #endif
+  return 0;
+}
