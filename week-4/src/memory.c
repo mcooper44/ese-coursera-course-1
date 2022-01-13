a932e163a8cda6f3806d2285cf2a10b192d85c00
Matt Cooper <m.cooper44@gmail.com>
2022-01-12 22:03:00 -0500

    added function prototypes to memory.c as placeholders while the logic gets worked out.

--- week-4/src/memory.c
+++ week-4/src/memory.c
@@ -20,7 +20,7 @@
  * @date April 1 2017
  *
  */
-#include "memory.h"
+#include "../include/common/memory.h"
 
 /***********************************************************
  Function Definitions
@@ -47,4 +47,19 @@ void set_all(char * ptr, char value, unsigned int size){
 void clear_all(char * ptr, unsigned int size){
   set_all(ptr, 0, size);
 }
+
+uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length);
+
+uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length);
+
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
+
+uint8_t * my_memzero(uint8_t * src, size_t length);
+
+uint8_t * my_reverse(uint8_t * src, size_t length);
+
+int32_t * reserve_words(size_t length);
+
+void free_words(int32_t * src);
+
 
