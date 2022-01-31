dbd84cd1049f79ba2a4392a568603c1b69fe0c10
Matt Cooper <m.cooper44@gmail.com>
2022-01-30 21:15:36 -0500

    added logic for all of the my_x functions

--- week-4/src/memory.c
+++ week-4/src/memory.c
@@ -25,49 +25,110 @@
 /***********************************************************
  Function Definitions
 ***********************************************************/
-void set_value(char * ptr, unsigned int index, char value){
+void set_value(char * ptr, unsigned int index, char value)
+{
   ptr[index] = value;
 }
 
-void clear_value(char * ptr, unsigned int index){
+void clear_value(char * ptr, unsigned int index)
+{
   set_value(ptr, index, 0);
 }
 
-char get_value(char * ptr, unsigned int index){
+char get_value(char * ptr, unsigned int index)
+{
   return ptr[index];
 }
 
-void set_all(char * ptr, char value, unsigned int size){
+void set_all(char * ptr, char value, unsigned int size)
+{
   unsigned int i;
-  for(i = 0; i < size; i++) {
+  for(i = 0; i < size; i++) 
+  {
     set_value(ptr, i, value);
   }
 }
 
-void clear_all(char * ptr, unsigned int size){
+void clear_all(char * ptr, unsigned int size)
+{
   set_all(ptr, 0, size);
 }
 
 // my functions
-uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length){
+uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length)
+{
+  // important to know that we are pointing to the same type
+  // to be compliant with the C standard
+  uint8_t *s_pt = src;
+  uint8_t *d_pt = dst;
 
-
+  // compare the memory addresses - are we going to clobber
+  // bits we are about to try and send? If not proceed forward
+  if (d_pt < s_pt) 
+  {
+    while (length--)
+    {
+      *d_pt++ = *s_pt++;
+    }
+  }
+  else
+  // move backwards to avoid clobbering the bits to send
+  {    
+    for(uint8_t i = (length -1); i>0; i--)
+    {
+      *(dst+i)-- = *(src+i)--;
+    }
+  }
+  return dst;
+}    
 
 uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length){
   unsigned int i = 0;
   for (i; i<length; i++){
-    *(dst+i) = *(src+i);
+    *dst++ = *src++;
   }
   return dst;
 }
-uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
 
-uint8_t * my_memzero(uint8_t * src, size_t length);
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value)
+{
+  uint8_t *s_p = src;
+  while (length--)
+  {
+    *s_p = value;
+  }
+  return src;
+}
+  
+uint8_t * my_memzero(uint8_t * src, size_t length)
+{
+  return my_memset(src, length, 0);
+}
 
-uint8_t * my_reverse(uint8_t * src, size_t length);
+uint8_t * my_reverse(uint8_t * src, size_t length)
+{
+  uint8_t *temp_buffer;
+  temp_buffer = (uint8_t*) malloc(length * sizeof(uint8_t));
 
-int32_t * reserve_words(size_t length);
+  for (uint8_t i=0; i<length; i++)
+  {
+    *(temp_buffer+i) = *(src+i);
+  }
+  while (length--)
+  {
+    *(src+length) = *((temp_buffer+length)-(length-1));
+  }
+  free(temp_buffer);
+  return(src);
+}
 
-void free_words(int32_t * src);
+int32_t * reserve_words(size_t length)
+{
+  return malloc(length * sizeof(length));
+}
+void free_words(int32_t * src)
+{
+  return free(src);
+}
 
 
