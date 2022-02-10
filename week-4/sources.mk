f63b3938d88701db3684bbb17a229a57695a8eb5
Matt Cooper <m.cooper44@gmail.com>
2022-02-09 22:57:40 -0500

    moved Makefile and sources ..

---
+++ week-4/sources.mk
@@ -0,0 +1,26 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+#*****************************************************************************
+
+HOST_SOURCES = ./src/main.c ./src/memory.c ./src/course1.c  ./src/stats.c
+
+MSP432_SOURCES =  ./src/main.c \
+		  ./src/memory.c \
+		  ./src/course1.c \
+		  ./src/stats.c \
+		  ./src/startup_msp432p401r_gcc.c \
+		  ./src/system_msp432p401r.c \
+		  ./src/interrupts_msp432p401r_gcc.c
+
+HOST_INCLUDES = -I../include/common
+
+MSP432_INCLUDES = -I../include/common \
+		  -I../include/msp432 \
+		  -I../include/CMSIS
