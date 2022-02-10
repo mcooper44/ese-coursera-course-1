83e5de80b61097ee4dfb16db4cdf23ca3071a4cc
Matt Cooper <m.cooper44@gmail.com>
2022-02-09 22:51:46 -0500

    cleaning up includes

--- week-4/src/course1.c
+++ week-4/src/course1.c
@@ -17,12 +17,7 @@
  *
  */
 
-#include <stdint.h>
-#include "course1.h"
-#include "platform.h"
-#include "memory.h"
-#include "data.h"
-#include "stats.h"
+#include "../include/common/course1.h"
 
 int8_t test_data1() {
   uint8_t * ptr;
