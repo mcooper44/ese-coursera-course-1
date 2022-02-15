13ed3f0ad5bde992c0056e0922c4e9b8dab11ba4
Matt Cooper <m.cooper44@gmail.com>
2022-02-14 22:01:01 -0500

    was using int32_t instead of uint32_t in free_words func which was causing a compiliation error

--- week-4/include/common/memory.h
+++ week-4/include/common/memory.h
@@ -170,7 +170,7 @@ int32_t * reserve_words(size_t length);
  * 2. All operations need to be performed using pointer arithmetic, not 
  * array indexing
 */
-void free_words(int32_t * src);
+void free_words(uint32_t * src);
 
 /********************************************************************/
 
--- week-4/src/memory.c
+++ week-4/src/memory.c
@@ -131,7 +131,7 @@ int32_t * reserve_words(size_t length)
 {
   return malloc(length * sizeof(length));
 }
-void free_words(int32_t * src)
+void free_words(uint32_t * src)
 {
   return free(src);
 }
